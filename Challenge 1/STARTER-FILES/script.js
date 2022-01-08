b512698b40faa97dd53f6e8ddc266c25b8b06aa8
Nguyen Thanh Tung <tungntfx08789@funix.edu.vn>
2022-01-08 08:30:28 +0700

    rename file

---
+++ Challenge 1/STARTER-FILES/script.js
@@ -0,0 +1,27 @@
+/*****************************************
+ * 
+ *          TOGGLE THE TIMER
+ * 
+ */
+// input
+const btnStart = document.body.querySelector('button.start')
+
+btnStart.addEventListener('click', toggleTimer)
+
+// output
+function toggleTimer() {
+
+    return true;
+}
+
+
+
+
+
+
+
+/*****************************************
+ * 
+ *     CHANGE THE LENGTH FOR THE TIMER
+ * 
+ */
