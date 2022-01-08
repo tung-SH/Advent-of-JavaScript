c70906a547c8a366d495379d1b485a3fdf225ae1
Nguyen Thanh Tung <tungntfx08789@funix.edu.vn>
2022-01-08 09:12:37 +0700

    trying building the toggle timer function

--- Challenge 1/STARTER-FILES/script.js
+++ Challenge 1/STARTER-FILES/script.js
@@ -8,11 +8,39 @@ const btnStart = document.body.querySelector('button.start')
 
 btnStart.addEventListener('click', toggleTimer)
 
-// output
-function toggleTimer() {
 
-    return true;
+// process
+const minutes = document.body.querySelector('.time .minutes input')
+const seconds = document.body.querySelector('.time .seconds input')
+
+function toggleTimer() {
+    let minutesValue = minutes.value;
+    let secondsValue = seconds.value;
+
+    let countDownTime = new Date(1000 * secondsValue * minutesValue).getTime();
+
+    var x = setInterval(countdown(countDownTime), 1000)
+
 }
+
+function countdown(countDownTime) {
+    if (countDownTime < 0) {
+        clearInterval(x)
+    }
+
+    countDownTime -= 1000
+    console.log(countDownTime)
+
+
+}
+
+  
+
+
+
+
+
+
 
 
 
