5629d10ff87e4c502be61d7f7aecb9fb5a45f489
Nguyen Thanh Tung <tungntfx08789@funix.edu.vn>
2022-01-11 08:32:02 +0700

    finish the startTimer function - step one more

--- Challenge 1/STARTER-FILES/script.js
+++ Challenge 1/STARTER-FILES/script.js
@@ -12,30 +12,50 @@ btnStart.addEventListener('click', toggleTimer)
 // process
 const minutes = document.body.querySelector('.time .minutes input')
 const seconds = document.body.querySelector('.time .seconds input')
+let isWorking = false;
 
+// dễ dàng như thế sao mình lại ko biết nhỉ 
 function toggleTimer() {
-    let minutesValue = minutes.value;
-    let secondsValue = seconds.value;
+    isWorking = !isWorking;
 
-    let countDownTime = new Date(1000 * secondsValue * minutesValue).getTime();
-
-    var x = setInterval(countdown(countDownTime), 1000)
-
-}
-
-function countdown(countDownTime) {
-    if (countDownTime < 0) {
-        clearInterval(x)
+    if (isWorking) {
+        startTimer()
+    } else {
+        stopTimer()
     }
-
-    countDownTime -= 1000
-    console.log(countDownTime)
-
-
 }
 
   
+// có 1 số trường hợp thì mình nên đặt hàm trong hàm 
+function startTimer() {
+    let minutesValue = minutes.value
+    let secondsValue = seconds.value
+    
+    var x = setInterval(minus, 1000);
 
+    function minus() { 
+        if (secondsValue == 0 && minutesValue == 0) { 
+            clearInterval(x)
+        } else if (secondsValue <= 0) {
+            minutesValue -= 1;
+            secondsValue = 59;
+        } else {
+            secondsValue -= 1;
+        }
+        
+        output(minutesValue, secondsValue)
+    } 
+}
+
+function output(minutesValue, secondsValue) {
+    minutes.value = minutesValue
+    seconds.value = secondsValue
+
+}
+// nãy mình mới học là có thể bỏ hàm vào trong hàm mà - khà khà - ko được
+function stopTimer() {
+    clearInterval(x)
+} 
 
 
 
