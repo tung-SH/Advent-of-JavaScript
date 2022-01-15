91e4c0e71a1b85ca282cd48c51ad4e59d3862276
Nguyen Thanh Tung <tungntfx08789@funix.edu.vn>
2022-01-15 10:00:12 +0700

    code again the first function of challenge 1

--- Challenge 1/STARTER-FILES/script.js
+++ Challenge 1/STARTER-FILES/script.js
@@ -3,69 +3,66 @@
  *          TOGGLE THE TIMER
  * 
  */
-// input
+
+// input 
 const btnStart = document.body.querySelector('button.start')
 
 btnStart.addEventListener('click', toggleTimer)
 
 
 // process
+const minutes = document.body.querySelector('.minutes input')
+const seconds = document.body.querySelector('.seconds input')
+
 let isWorking = false;
 
+let x = 'variable stores the interval'
+
+
 function toggleTimer() {
-    isWorking = !isWorking;
+    isWorking = !isWorking
 
     if (isWorking) {
         startTimer()
     } else {
-        console.log("not working")
         stopTimer()
     }
 }
 
-  
-// có 1 số trường hợp thì mình nên đặt hàm trong hàm 
-const minutes = document.body.querySelector('.time .minutes input')
-const seconds = document.body.querySelector('.time .seconds input')
 
 function startTimer() {
     let minutesValue = minutes.value
     let secondsValue = seconds.value
-    
-    let x = setInterval(minus, 1); //mình muốn nó ở lớp ngoài 
 
-    function minus() { 
-        if (secondsValue == 0 && minutesValue == 0) { 
-            clearInterval(x)
-        } else if (secondsValue <= 0) {
-            minutesValue -= 1;
-            secondsValue = 59;
-        } else {
-            secondsValue -= 1;
+    x = setInterval(minus, 10)
+
+    function minus() {
+        if (secondsValue == 0 && minutesValue == 0) {
+            stopTimer()
         }
-        
+        else if (secondsValue == 0) {
+            minutesValue -= 1
+            secondsValue = 59
+        }
+        else {
+            secondsValue -= 1
+        }
+
         output(minutesValue, secondsValue)
-    } 
+    }
 }
-
-function output(minutesValue, secondsValue) {
-    minutes.value = minutesValue
-    seconds.value = secondsValue
-
-}
-// nãy mình mới học là có thể bỏ hàm vào trong hàm mà - khà khà - ko được
 function stopTimer() {
-    console.log('inside stopTimer')
-
-    // sai từ đoạn này
     clearInterval(x)
-} 
+
+}
 
 
+// output
+function output(minutesValue, secondsValue) {
+    minutes.value = (minutesValue < 10) ? '0' + minutesValue : minutesValue
+    seconds.value = (secondsValue < 10) ? '0' + secondsValue : secondsValue
 
-
-
-
+}
 
 
 
